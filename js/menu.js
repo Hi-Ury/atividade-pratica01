407521d9a9360f8583b06755f4b7a5715fec3613
Hiury Matheus <hiury2204@gmail.com>
2025-10-03 17:25:41 -0300

    responsividade
    Terminei a responsividade da pagina home

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var links = window.document.getElementById('conteudo')
+
+function AbrirMenu() {
+    links.style.width = '100%'
+    links.style.left = '20%'
+}
+
+function FucharMenu() {
+    links.style.width = 0
+    links.style.left = '100%'
+}
+
